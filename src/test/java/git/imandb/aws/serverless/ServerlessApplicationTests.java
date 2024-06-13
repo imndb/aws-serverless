07c229453bd9ac522f48767d5212fba8e49a74fa
Iman Dabbaghi <iman.dabbaghi-divshali@t-systems.com>
2024-06-13 18:16:16 +0200

    init

---
+++ src/test/java/git/imandb/aws/serverless/ServerlessApplicationTests.java
@@ -0,0 +1,13 @@
+package git.imandb.aws.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
